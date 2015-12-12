60ffabf0161a5d5fdf69caf2192050f02cc76e12
Nan Ma <nma@yammer-inc.com>
2015-12-11 20:39:53 -0800

    Revert "@klq Not sure if this is correct."
    This reverts commit 621c4091f7277e2f454fe93508c5cbfc1ed52af0.

--- test.rb
+++ test.rb
@@ -11,12 +11,11 @@ left_vertical_line = Line.perp_bis([origin, right_point])
 golden_section = (1 - (Math.sqrt(5) - 1)/2)
 
 right_vertical_line = Line.perp_bis([right_point, Point.new([golden_section, 0])])
-bottom_point = right_vertical_line.intersection_with_circle(circle).first
 
 initial_layout = Layout.new([origin, right_point], [horizontal_line], [circle])
 initial_layout.add_entity(left_vertical_line)
 
-targets = [bottom_point]
+targets = [right_vertical_line]
 
 # steps = [:compass, :compass, :perp_bis] # original
 
@@ -24,7 +23,7 @@ first_moves = [:line, :circle, :perp_bis, :perp, :parallel, :compass]
 second_moves = [:line, :perp_bis, :perp, :parallel]
 
 first_moves.each do |first_move|
-  first_moves.each do |second_move|
+  second_moves.each do |second_move|
     steps = [first_move, second_move]
 
     task = Task.new(initial_layout, targets, steps)
@@ -35,7 +34,7 @@ first_moves.each do |first_move|
       p steps
 
       solution_layout.print(targets)
-      break
+      exit
     end
   end
 end
