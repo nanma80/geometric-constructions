29e26dc1f48d41a82e10455eb71c31a9622c4a19
Nan Ma <nma@yammer-inc.com>
2016-02-25 13:16:24 -0800

    new learnings

--- tasks/54-degree-attempt3-5L.rb
+++ tasks/54-degree-attempt3-5L.rb
@@ -15,26 +15,28 @@ up_point = Point.new([up_distance * Math.cos(theta), up_distance * Math.sin(thet
 line2 = Line.new([center_point, up_point])
 
 initial_layout = Layout.new([center_point, right_point], [horizontal_line], [])
+initial_layout << Line.new([Point.new([0.5, 0]), Point.new([0.5, 1])])
+initial_layout << Circle.new(Point.new([0.5, 0]), Point.new([0.5, 1]))
+initial_layout << Circle.new(Point.new([1.5, 0]), Point.new([1.5 - Math.sqrt(5) * 0.5, 0]))
+
 filters = {
-  # 1 => [Circle.new(right_point, center_point)],
-  4 => [Point.new([(3-Math.sqrt(5))/2, 0])]
 }
 targets = [line2]
 
 l_moves = [:circle, :line, :perp_bis, :perp, :parallel, :angle_bis, :compass]
 line_moves = [:line, :perp_bis, :perp, :parallel, :angle_bis]
 
-step_count = 5
+step_count = 2
 generator_max = l_moves.length ** step_count
 
-(1371..(generator_max - 1)).each do |generator|
+(0..(generator_max - 1)).each do |generator|
   steps = []
   generator_string = (generator + generator_max).to_s(l_moves.length)[1..step_count]
   generator_string.split('').each do |step_id|
     steps << l_moves[step_id.to_i]
   end
   Logger.log [generator, steps].inspect
-  next unless steps.include?(:angle_bis) # official tweet announced that we needed angle bis
+  # next unless steps.include?(:angle_bis) # official tweet announced that we needed angle bis
   next unless line_moves.include?(steps.last)
 
   task = Task.new(initial_layout, targets, steps, filters)
--- tasks/regular_pentagon_9L.rb
+++ tasks/regular_pentagon_9L.rb
@@ -1,5 +1,7 @@
 # regular pentagon
 # We learned that it's impossible to establish a circle in 4L that is centered at (0, -1) and passing bottom_point
+# We learned that it's impossbiel to establish up_right vertex in 4L
+
 require './geometric-constructions'
 
 origin = Point.new([0, 0])
@@ -14,7 +16,9 @@ down_right = Point.new([Math.sin(2*theta), Math.cos(2*theta)])
 bottom_point = Line.new([origin, top_point]).intersection_with_line(Line.new([up_right, down_right])).first
 target_circle_center = Point.new([0, -1])
 target_circle = Circle.new(target_circle_center, bottom_point)
-filters = {3 => [target_circle_center ]}
+# filters = {3 => [target_circle_center ]}
+# filters = {}
+filters = {1 => [ Line.new([origin, top_point]) ]}
 
 l_moves = [:circle, :line, :perp_bis, :perp, :parallel, :angle_bis, :compass]
 line_moves = [:line, :perp_bis, :perp, :parallel, :angle_bis]
@@ -24,16 +28,17 @@ step_count = 4
 generator_max = l_moves.length ** step_count
 
 initial_layout = Layout.new([origin, top_point], [], [circle])
-targets = [target_circle]
+# targets = [target_circle]
+targets = [bottom_point]
 
-(63..(generator_max - 1)).each do |generator|
+(0..(generator_max - 1)).each do |generator|
   steps = []
   generator_string = (generator + generator_max).to_s(l_moves.length)[1..step_count]
   generator_string.split('').each do |step_id|
     steps << l_moves[step_id.to_i]
   end
-  p [generator, steps]
-  next unless circle_moves.include? steps.last
+  Logger.log "\e[33m#{[generator, steps].inspect}\e[0m"
+  # next unless circle_moves.include? steps.last
 
   task = Task.new(initial_layout, targets, steps, filters)
 
