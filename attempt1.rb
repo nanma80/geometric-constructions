cdac5386a3ccea8cd94b0061b5778059acd201c9
Nan Ma <nma@c02jl01kdkq4.guest.corp.microsoft.com>
2018-01-23 18:56:28 -0800

    Still not solved. update

--- attempt1.rb
+++ attempt1.rb
@@ -1,17 +1,20 @@
 # 14.4 6L attempt
+# Attempt 0: assume step1 = Line.new([right_bottom, top]) and search for 4 steps (mistake)
+# Didn't find anything until 419
 # Attempt 1: Failed. assume step1 = Line.new([right_bottom, top]) 
 # and the last two steps are a circle to find the other point and connecting them
 # Failed in two ways
 # Attempt 2: don't assume first step. Search for 4 steps. Searched up to 97
 # Attempt 3: assume step1 = Circle.new(left_bottom, top). Search for 3 steps. 
 # Didn't find anything with target = origin (0.3 sample rate)
-# for target = middle_left_line, didn't find anything until 95
+# for target = middle_left_line, didn't find anything
+
 
 require './geometric-constructions'
 
 origin = Point.new([0, 0]).with_name('origin')
 right_bottom = Point.new([1, 0]).with_name('right bottom')
-theta_1 = 126.93 * Math::PI / 180
+theta_1 = 122.6 * Math::PI / 180
 theta_2 = 34 * Math::PI / 180
 middle_left_line = Point.new([Math.cos(theta_1), Math.sin(theta_1)])
 top = Point.new([Math.cos(theta_1) + Math.cos(theta_2), Math.sin(theta_1) + Math.sin(theta_2)]).with_name('top')
@@ -25,23 +28,23 @@ left_line_segment = LineSegment.new([left_bottom, top]).with_name('left line seg
 
 initial_layout = Layout.new([left_bottom, top, right_bottom], [left_line_segment, bottom_line_segment], [])
 
-step1 = Circle.new(left_bottom, top)
-new_point = step1.intersection_with_line(bottom_line_segment).first
-p left_bottom.coordinates
-p new_point.coordinates
+# step1 = Circle.new(left_bottom, top)
+# new_point = step1.intersection_with_line(bottom_line_segment).first
+# p left_bottom.coordinates
+# p new_point.coordinates
 
-initial_layout << step1
+# initial_layout << step1
 
-# targets = [origin]  # or middle_left_line
-targets = [middle_left_line]
+targets = [origin]  # or middle_left_line
+# targets = [middle_left_line]
 
 l_moves = [:line, :circle, :perp_bis, :perp, :parallel, :angle_bis, :compass] # up to 97
 
-step_count = 3
+step_count = 4
 generator_max = l_moves.length ** step_count
 
-(0..generator_max).each do |generator|
-# (189..200).each do |generator|
+(61..generator_max).each do |generator|
+# (188..200).each do |generator|
   steps = []
   generator_string = (generator + generator_max).to_s(l_moves.length)[1..step_count]
   generator_string.split('').each do |step_id|
@@ -57,8 +60,6 @@ generator_max = l_moves.length ** step_count
   else
     puts "*************Found solution*************"
     p steps
-    p left_bottom.coordinates
-    p new_point.coordinates
     solution_layout.print(targets)
     exit
   end
